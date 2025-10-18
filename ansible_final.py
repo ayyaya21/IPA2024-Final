b02a5c07f271368152b9d125d9ec5ff2d92f31c0
ayyaya21 <66070221@kmitl.ac.th>
2025-10-18 21:20:16 +0700

    Finally, we escaped

--- ansible_final.py
+++ ansible_final.py
@@ -2,7 +2,8 @@ import subprocess
 
 def showrun():
     # read https://www.datacamp.com/tutorial/python-subprocess to learn more about subprocess
-    command = ['ansible-playbook', 'playbook.yaml']
+    command = ['ansible-playbook', 'playbook.yaml', '--flush-cache']
+
     result = subprocess.run(command, capture_output=True, text=True)
     result = result.stdout
     if 'ok=2' in result:
