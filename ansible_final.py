4200ca6d32a8969cf9bd25ce27633ecf21cba8be
ayyaya21 <66070221@kmitl.ac.th>
2025-10-18 19:12:02 +0700

    update: fix ansible

--- ansible_final.py
+++ ansible_final.py
@@ -1,15 +1,13 @@
 import subprocess
 
-STUDENT_ID = "66070221"
-
 def showrun():
-    playbook_file = "playbook.yaml"
-    command = ["ansible-playbook", playbook_file]
+    output_filename = "show_run_66070221_R1-Exam.txt"
+    command = ['ansible-playbook', 'playbook.yaml']
+    
     result = subprocess.run(command, capture_output=True, text=True)
-    output = result.stdout + result.stderr
-    if "FAILED=0" in output:
-        router_name = "R1-Exam"
-        filename = f"show_run_{STUDENT_ID}_{router_name}.txt"
-        return filename
+    result = result.stdout
+    
+    if 'ok=2' in result:
+        return output_filename
     else:
-        return "Error: Ansible"
+        return 'Error: Ansible'
