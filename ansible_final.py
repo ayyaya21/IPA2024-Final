9653f1d8883810652121965bb07507163eaa06f5
ayyaya21 <66070221@kmitl.ac.th>
2025-10-18 20:47:36 +0700

    update: ansible

--- ansible_final.py
+++ ansible_final.py
@@ -2,7 +2,7 @@ import subprocess
 
 def showrun():
     # read https://www.datacamp.com/tutorial/python-subprocess to learn more about subprocess
-    command = ['ansible-playbook', 'playbook.ymal']
+    command = ['ansible-playbook', 'playbook.yaml']
     result = subprocess.run(command, capture_output=True, text=True)
     result = result.stdout
     if 'ok=2' in result:
